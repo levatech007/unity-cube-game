7efe37548d5984cb500522f57905b50616e7a10d
Eva <evaliisagalen@gmail.com>
2018-11-08 17:55:20 -0800

    initial commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	//reference to the player position
+	public Transform player;
+	public Vector3 offset;
+	// Update is called once per frame
+	void Update () {
+		//set camera position to player position
+		transform.position = player.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+ using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float forceApplied = 20f; //force added to the player
+	public Rigidbody rb; //player
+	public float forwardForce = 1000f; //for the player to move forward when game starts
+
+	//update is called once per physics update
+	void FixedUpdate() {
+		//what key did the user press?
+		//move forward
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+		//move right
+		if(Input.GetKey("d")) {
+      Debug.Log("D clicked!");
+			rb.AddForce(forceApplied * Time.deltaTime, 0, 0);
+		}
+		//move left
+		if(Input.GetKey("a")) {
+      Debug.Log("A clicked!");
+			rb.AddForce(-forceApplied * Time.deltaTime, 0, 0);
+		}
+
+	}
+}
